0dbee5d9a5e86e5b822209c0efe6b3af6229d658
Dylan J <dylan@teamtritan.wtf>
2020-11-24 18:24:20 -0500

    renaming views cat

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ route.get('/status', async (req, res, next) => {
 });
 
 route.get('*', async (req, res, next) => {
-    res.render('codes/unauthorized')
+    res.render('errors/unauthorized')
     //res.send({http_code:"403",content:"Access Denied","go_back":"https://teamtritan.wtf"})
 });
 

022fd32b56082b1e7cfa3bf8e519822a4274555a
Dylan J <dylan@teamtritan.wtf>
2020-11-24 16:18:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.static(__dirname + "/public"));
 
 (async () => {
     
-app.listen(config.port || 3000, () => {
+app.listen(config.port, () => {
         console.log(chalk.greenBright('[WEB STARTUP]'), `Web running on port ${config.port}.`);
     })
 })();

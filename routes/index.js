8ebce3e5f0983b18d03990a8177b408ad8303c06
Dylan J <dylan@teamtritan.wtf>
2020-11-24 16:31:50 -0500

    .

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const {Router} = require('express');
 var route = Router();
 
-route.get('/', async (req, res, next) => {
-    res.send({http_code:"403",content:"Access Denied"})
+route.get('/status', async (req, res, next) => {
+    res.send({status:"alive"})
 });
 
 route.get('*', async (req, res, next) => {
-    res.send({http_code:"403",content:"Access Denied"})
+    res.send({http_code:"403",content:"Access Denied","go_back":"https://teamtritan.wtf"})
 });
 
 module.exports = route;

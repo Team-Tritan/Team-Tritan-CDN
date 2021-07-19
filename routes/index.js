6b10d6b3f193f194f6f0779674a01ee488f8ccdf
Dylan James <dylan@suhd00d.co>
2021-07-19 19:38:11 -0400

    503 page

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,21 @@
-const {Router} = require('express');
+const { Router } = require("express");
 var route = Router();
 
-route.get('/', async (req, res, next) => {
-    res.redirect('https://tritan.gg')
+route.get("/", async (req, res, next) => {
+  res.redirect("https://tritan.gg");
 });
 
-route.get('/status', async (req, res, next) => {
-    res.send({status:"alive"})
+route.get("/status", async (req, res, next) => {
+  res.send({ status: "alive" });
 });
 
-route.get('*', async (req, res, next) => {
-    res.render('errors/unauthorized')
-    //res.send({http_code:"403",content:"Access Denied","go_back":"https://teamtritan.wtf"})
+route.get("/503", async (req, res, next) => {
+  res.render("errors/503");
+});
+
+route.get("*", async (req, res, next) => {
+  res.render("errors/unauthorized");
+  //res.send({http_code:"403",content:"Access Denied","go_back":"https://teamtritan.wtf"})
 });
 
 module.exports = route;
